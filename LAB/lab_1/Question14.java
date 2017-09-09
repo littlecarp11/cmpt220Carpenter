87efc728206198f393be50f2713478dd149d29bf
Juan Arias <juan.arias@marist.edu>
2017-09-09 13:14:43 -0400

    Feedback

--- LAB/lab_1/Question14.java
+++ LAB/lab_1/Question14.java
@@ -3,24 +3,29 @@ public class Question14 {
 
 	public static void main(String[] args) {
 		// Make a table 
+		// Calling the method
+		prac(3);		
 	}
-		public static void prac(int a){
-			System.out.printf("% - 5s" , "a^2"); 
+	
+	public static void prac(int a){ // JA: If you put everything inside a method, you have to call it
+		System.out.printf("%5s" , "a^2"); 
 
-			System.out.printf("% - 5s" , "a^2"); 
+		System.out.printf("%5s" , "a^2"); 
 
-			System.out.printf("% - 5s" , "a^3"); 
+		System.out.printf("%5s" , "a^3"); 
+
+		System.out.println();
+		for(int i = 1; i <= a; i++){
+
+			System.out.printf("%5d", i); 
+
+			System.out.printf("%5d",i*i);
+
+			System.out.printf("%5d",i*i*i);
 
 			System.out.println();
-			for(int i = 1; i <= a; i++){
-
-				System.out.printf("% - 5d"); 
-
-				System.out.printf("% - 5di");
-
-				System.out.printf("% - 5di*i");
-
-				System.out.println();
-			}
 		}
 	}
+		
+
+}
